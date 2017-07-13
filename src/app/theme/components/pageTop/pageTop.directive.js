653ab676d0c38139db40e931036bbbbd02110c77
immadimtiaz <immadimtiaz@gmail.com>
2017-07-13 19:25:30 +0500

    changes

--- src/app/theme/components/pageTop/pageTop.directive.js
+++ src/app/theme/components/pageTop/pageTop.directive.js
@@ -9,7 +9,7 @@
       .directive('pageTop', pageTop);
 
   /** @ngInject */
-  function pageTop(authentication, $state) {
+  function pageTop(authentication, $state, dropDownConfig) {
     return {
       restrict: 'E',
       link: link,
@@ -17,6 +17,23 @@
     };
 
     function link(scope, elem, attrs){
+      _init();
+
+      function _init(){
+        scope.searchOptions = [
+          {name: 'International Trade By File Id', id: 'int_trade_file_id'},
+          {name: 'International Trade By BL/No.', id: 'int_trade_bl_no'},
+          {name: 'International Trade By Contract Number', id: 'int_trade_contract_id'},
+          {name: 'International Trade By Invoice No.', id: 'int_trade_invoice_no'}
+        ];
+        vm.searchObj = {
+          query: '',
+
+
+        }
+        scope.searchTypeConfig = dropDownConfig.getBasicDropDownConfig(false);
+      }
+
       scope.goToSettings = function(){
         $state.go('dashboard.settings.transactionStatus');
       };
